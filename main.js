82edbb1ffcec7460abda603694237bd45220b7ba
Adrian <adrian@Adrians-MBP.fritz.box>
2020-05-09 02:10:14 +0200

    - updated style - createt error message for more than four groups - bug fixes

--- main.js
+++ main.js
@@ -11,11 +11,25 @@ const res = await fetch('data.json');
 return res.json();
 }
 
+//linebreaker for more then two groups
+function linebreaker() {
+  let breaker = document.createElement("div");
+  breaker.setAttribute("class", "w-100");
+  document.getElementById("rusty_racer").appendChild(breaker);
+}
+
+//set counter numbers
+function setCounter(second, minute, hour, c, div_height, status, status_class){
+    document.getElementById("race" + c ).innerHTML = "<p class='my-auto text-center w-100'>" + hour + ":" + minute + ":" + second + status +"</p>";
+    document.getElementById("race" + c ).setAttribute("class", "row col text-center align-items-center p-0 m-0 " + div_height + status_class);
+}
+
 window.onload = async function() {
 
   // variables declaration
   var time_groupe = await loadJSON();
   var g = time_groupe.groupe_count;
+  var div_height = "h-100";
   var i = [];
   var p = [];
   var countDownDate = [];
@@ -23,17 +37,34 @@ window.onload = async function() {
   var minutes = [];
   var seconds = [];
   var distance = [] ;
-  let starttext = "loading";
+  
+  //checks if there are more than four groups
+  if (g > 4){
+    var para = document.createElement("div");
+    para.setAttribute("class", "row col text-center p-0 m-0");
+    para.innerHTML = "There are more than four groups defined!"
+    document.getElementById("rusty_racer").appendChild(para);
+    throw Error("There are more than four groups defined!");
+  } 
+  //changes height from div when more than two groups
+  else if (g > 2){
+    div_height = "h-50"
+  }
+
   for (var c = 0; c < g; c++){
     i[c] = 0;
     p[c] = "start";
+    if (c == 2){
+      linebreaker();
+    }
     var para = document.createElement("div");
     para.setAttribute("id", "race" + c);
-    para.setAttribute("class", "row col text-center");
+    para.setAttribute("class", "row col text-center p-0 m-0");
     document.getElementById("rusty_racer").appendChild(para);
-    document.getElementById("race" + c).innerText = '<p class="mx-auto">' + starttext + "</p>";
+    //document.getElementById("race" + c).innerText = '<p class="mx-auto">' + starttext + "</p>";
     countDownDate[c] = get_time(time_groupe, i, p, c);
   }
+
 
    
   // Update the count down every 1 second
@@ -57,15 +88,12 @@ window.onload = async function() {
     if (distance[c] > 0){
       if (p[c] == "start"){
         if (minutes[c] < 10 ){
-         document.getElementById("race" + c ).innerHTML = "<p class='my-auto text-center w-100'>" + hours[c] + ":" + minutes[c] + ":" + seconds[c] + "<br>Start Engine</p>";
-         document.getElementById("race" + c ).setAttribute("class", "row col text-center bg-warning text-dark align-items-center");
+          setCounter(seconds[c], minutes[c], hours[c], c, div_height,"<br>Start Engine", " bg-warning text-dark");
         }else{
-          document.getElementById("race" + c ).innerHTML = "<p class='my-auto text-center w-100'>" + hours[c] + ":" + minutes[c] + ":" + seconds[c] + "</p>";
-          document.getElementById("race" + c ).setAttribute("class", "row col text-center align-items-center");
+          setCounter(seconds[c], minutes[c], hours[c], c, div_height,"", "");
         }
       }else{
-        document.getElementById("race" + c ).innerHTML = "<p class='my-auto text-center w-100'>" + hours[c] + ":" + minutes[c] + ":" + seconds[c] + "<br>RACE</p>";
-        document.getElementById("race" + c ).setAttribute("class", "row col text-center bg-success text-white align-items-center");
+        setCounter(seconds[c], minutes[c], hours[c], c, div_height,"<br>RACE", "bg-success text-white");
       }
     }else{
       
@@ -79,7 +107,7 @@ window.onload = async function() {
       if (time_groupe.group[c].journey_times.length == i[c]) {
           //clearInterval(x);
           document.getElementById("race" + c ).innerHTML = "<p class='mx-auto'>Race Over</p>";
-          document.getElementById("race" + c ).setAttribute("class", "row col text-center bg-danger text-white align-items-center");
+          document.getElementById("race" + c ).setAttribute("class", "row col text-center bg-danger text-white align-items-center p-0 m-0 " + div_height);
 
       }else{
           
